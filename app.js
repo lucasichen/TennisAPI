8604fcd7ab3c1bbf1315b6b0b767162317865575
Lucas Chen <lucas.chen2016@gmail.com>
2023-12-10 20:53:20 -0500

    chore: attempt to fix vercel deployment

--- app.js
+++ app.js
@@ -6,12 +6,23 @@ require('dotenv').config();
 const cors = require('cors');
 app.use(cors());
 
-// Serve static files (optional)
-app.use(express.static('public'));
 
 // Set up Firebase Admin SDK
 const admin = require('firebase-admin');
-const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+const serviceAccount = {
+  "type": "service_account",
+  "project_id": process.env.FIREBASE_PROJECT_ID,
+  "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+  "private_key": process.env.FIREBASE_PRIVATE_KEY,
+  "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+  "client_id": process.env.FIREBASE_CLIENT_ID,
+  "auth_uri": process.env.FIREBASE_AUTH_URI,
+  "token_uri": process.env.FIREBASE_TOKEN_URI,
+  "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  "universe_domain": process.env.FIREBASE_UNIVERSE_DOMAIN,
+}
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
