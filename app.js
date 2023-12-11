f12447965c919e22bc8d176410668ad08d92f045
Lucas Chen <lucas.chen2016@gmail.com>
2023-12-10 20:30:15 -0500

    feat: add vercel config

--- app.js
+++ app.js
@@ -155,3 +155,6 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+// Export the Express API
+module.exports = app;
